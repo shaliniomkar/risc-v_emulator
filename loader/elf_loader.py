6a701f045f26d1ec51323e9403e3f4dacb444d88
Shalini Omkar <shaliniomkar@gmail.com>
2026-06-22 22:55:21 +0530

    elf loader

---
+++ loader/elf_loader.py
@@ -0,0 +1,36 @@
+from elftools.elf.elffile import ELFFile
+from cpu.cpu import CPU
+
+class ELFLoader:
+    def __init__(self):
+        self.cpu = CPU()
+
+    def load(self, filename):
+        with open(filename, 'rb') as f:
+            elf = ELFFile(f)
+
+            if elf.header['e_machine'] != "EM_RISCV":
+                raise ValueError("File is not risc-v binary.")
+            if elf.elfclass != 32:
+                raise ValueError("Not 32 bit binary.")
+            if not elf.little_endian:
+                raise ValueError("Not little-endian encoded.")
+
+            for segment in elf.iter_segments():
+                if segment.header.p_type == 'PT_LOAD':
+                    vaddr = segment.header.p_vaddr
+                    files = segment.header.p_filesz
+                    mems = segment.header.p_memsz
+                    data = segment.data()
+
+                    if mems > files:
+                        zero_count = mems - files
+                        zero_start = vaddr + files
+                        self.cpu.mem.load(zero_start, bytearray(zero_count))
+
+                    self.cpu.mem.load(vaddr, data)
+
+            self.cpu.pc = elf.header['e_entry']
+            
+    
+
